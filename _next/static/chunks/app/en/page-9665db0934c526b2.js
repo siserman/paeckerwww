a615b6d9850aa5c60baae334d9dd4282e6bc2bf2
Stephan Isermann <siserman@users.noreply.github.com>
2025-05-24 16:55:43 +0200

    lang dect fix

---
+++ _next/static/chunks/app/en/page-9665db0934c526b2.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[496,792],{9018:(e,s,_)=>{Promise.resolve().then(_.bind(_,3487))}},e=>{var s=s=>e(e.s=s);e.O(0,[485,724,487,441,517,358],()=>s(9018)),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/page-f44147ec020a322c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{5088:(e,s,_)=>{Promise.resolve().then(_.bind(_,3487))}},e=>{var s=s=>e(e.s=s);e.O(0,[485,724,487,441,517,358],()=>s(5088)),_N_E=e.O()}]);
